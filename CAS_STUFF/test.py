e1c50b7c1b7542ffa59893fec080c32990de4cd8
Casperliuliuliu <casperliu1118@gmail.com>
2023-12-01 17:46:25 +0800

    test api

---
+++ CAS_STUFF/test.py
@@ -0,0 +1,14 @@
+import sys
+path = "/Users/liushiwen/Desktop/大四上/物件導向程式設計/E-health"
+sys.path.append(f'{path}/include')
+from API import DB_API
+from API import DB_factory
+
+def main():
+    api = DB_API()
+    api.set_DB(DB_factory.create(DB_factory.plain_text_DB), f"{path}/DB/central_DB")
+    info = api.load_info("0001")
+    print(str(info))
+
+if __name__ == "__main__":
+    main()
