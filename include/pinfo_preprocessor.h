72de1af0ab933d610320e5daa3fd15e7b9819329
XDOAO <a28615781a@gmail.com>
2023-12-02 00:52:40 +0800

    Preprocess completed. Not test yet.

---
+++ include/pinfo_preprocessor.h
@@ -0,0 +1,134 @@
+#ifndef __PINFO_PREPROCESSOR_H__
+#define __PINFO_PREPROCESSOR_H__
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include "pinfo.h"
+
+class CheckerModule{
+public:
+    void check_last(patient_info_t& patient_info){
+        size_t med_records_size = patient_info.get_med_records().size();
+        check(patient_info, med_records_size-1);
+
+    }
+    void check_all(patient_info_t& patient_info){
+        size_t med_records_size = patient_info.get_med_records().size();
+        for(int i=0;i<med_records_size;++i){
+            check(patient_info, i);
+        }
+    }
+    virtual bool check(patient_info_t&,int) = 0;//True: Error been found False: No Error found
+};
+
+class BMICheckerModule: private CheckerModule{
+public:
+    bool check(patient_info_t& patient_info,int idx) override{
+        std::vector<med_record_t> med_records = std::move(patient_info.get_med_records());
+        med_record_t med_record = std::move(med_records[idx]);
+        reports_t reports = std::move(med_record.get_reports());
+        std::map<std::string,double>  numerical_record = reports.get_numerical_records();
+        if(numerical_record.find("BMI") != numerical_record.end() && numerical_record["BMI"] > 6 && numerical_record["BMI"] < 300){
+            return false;
+        }
+        double height,weight;
+        for(auto& [key,value]:numerical_record){
+            if(key.find("height") != std::string::npos) height = value;
+            if(key.find("weight") != std::string::npos) weight = value;
+        }
+        numerical_record["BMI"] = weight / height / height;
+        reports.set_numerical_records(numerical_record);
+        med_record.set_reports(reports);
+        med_records[idx] = med_record;
+        patient_info.set_med_records(med_records);
+        return true;
+        
+    }
+};
+
+class BMICheckerModule: public CheckerModule{
+public:
+    bool check(patient_info_t& patient_info,int idx) override{
+        std::vector<med_record_t> med_records = std::move(patient_info.get_med_records());
+        med_record_t med_record = std::move(med_records[idx]);
+        reports_t reports = std::move(med_record.get_reports());
+        std::map<std::string,double>  numerical_record = reports.get_numerical_records();
+        if(numerical_record.find("BMI") != numerical_record.end() && std::isnan(numerical_record["BMI"]) && numerical_record["BMI"] > 6 && numerical_record["BMI"] < 300){
+            return false;
+        }
+        double height,weight;
+        for(auto& [key,value]:numerical_record){
+            if(key.find("height") != std::string::npos) height = value;
+            if(key.find("weight") != std::string::npos) weight = value;
+        }
+        numerical_record["BMI"] = weight / height / height;
+        reports.set_numerical_records(numerical_record);
+        med_record.set_reports(reports);
+        med_records[idx] = med_record;
+        patient_info.set_med_records(med_records);
+        return true;
+    }
+};
+
+class IsNANCheckerModule: public CheckerModule{
+public:
+    bool check(patient_info_t& patient_info,int idx) override{
+        std::vector<med_record_t> med_records = std::move(patient_info.get_med_records());
+        med_record_t med_record = std::move(med_records[idx]);
+        reports_t reports = std::move(med_record.get_reports());
+        std::map<std::string,double>  numerical_record = reports.get_numerical_records();
+        bool isFoundNAN = false;
+        for(auto& [key,value]:numerical_record){
+            if(std::isnan(value)){
+                value = 0.0;
+                isFoundNAN = true;
+            }
+        }
+        if(!(isFoundNAN)){
+            return false;
+        }
+        else{
+            reports.set_numerical_records(numerical_record);
+            med_record.set_reports(reports);
+            med_records[idx] = med_record;
+            patient_info.set_med_records(med_records);
+        }
+        return true;
+        
+    }
+};
+
+class Preprocessor{
+public:
+    patient_info_t& patient_info;
+    Preprocessor() = default;
+    Preprocessor(std::vector<CheckerModule>& checkers_,patient_info_t& patient_info_):
+        checkers{std::vector<CheckerModule>()},
+        patient_info{patient_info_}
+    {
+        checkers.resize(checkers_.size());
+        for(int i=0;i < checkers_.size() ; ++i){
+            checkers[i] = checkers_[i];
+        }
+    }
+    bool preprocess(){ //True if error found in preprocess progress
+        for(auto& x:checkers){
+            x.check_last(patient_info);
+        }
+    }
+    bool preprocess(int idx){
+        for(auto& x:checkers){
+            x.check(patient_info,idx);
+        }
+    }
+    bool preprocess_all(){
+        for(auto& x:checkers){
+            x.check_all(patient_info);
+        }
+    }
+private:
+    std::vector<CheckerModule> checkers;
+};
+
+#endif
